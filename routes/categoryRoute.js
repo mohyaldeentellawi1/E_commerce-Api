916d4ad4b0cd39fff8e51cc524b5d9425ce97913
mohyaldeen <mohyaldeentellawi@gmail.com>
2024-07-24 23:23:35 +0300

    Add multer and sharp for image processing

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const subCategoryRoute = require('./subCategoryRoute');
-const { getCategories , createCategory , getCategory , updateCategory , deleteCategory } = require('../services/categorySevices');
+const { getCategories , createCategory , getCategory , updateCategory , deleteCategory , uploadCategoryImage , imageProcessing} = require('../services/categorySevices');
 const { getCategoryValidator, createCategoryValidator ,updateCategoryValidator , deleteCategoryValidator } = require('../utils/validators_rules/categoryValidatorRules');
 const router = express.Router();
+
+
 
 
 // get subcategories of a main category
 router.use('/:categoryId/subcategories', subCategoryRoute);
 
-router.route('/').get(getCategories).post(createCategoryValidator, createCategory);
+router.route('/').get(getCategories).post(uploadCategoryImage, imageProcessing , createCategoryValidator, createCategory);
 router.route('/:id')
 .get(getCategoryValidator,getCategory)
 .put(updateCategoryValidator, updateCategory)
