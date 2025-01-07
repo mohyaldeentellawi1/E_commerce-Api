2a93d64f4c7090d50ffb3401f005c3b6790b9f53
mohyaldeentellawi <mohyaldeentellawi@gmail.com>
2025-01-07 17:19:39 +0300

    Refactor product model for improved readability and consistency

--- models/productModel.js
+++ models/productModel.js
@@ -1,116 +1,118 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 // Create a Product Schema
-const productSchema = new mongoose.Schema({
-    title:{
-        type: String,
-        required: [true, 'Product title is required'],
-        trim: true,
-        maxlength: [100, 'Product title must not exceed 100 characters'],
-        minlength: [5, 'Product title must be at least 5 characters']
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Product title is required"],
+      trim: true,
+      maxlength: [100, "Product title must not exceed 100 characters"],
+      minlength: [5, "Product title must be at least 5 characters"],
     },
     slug: {
-        type: String,
-        // lowercase: true,
+      type: String,
+      required: true,
+      lowercase: true,
     },
     description: {
-        type: String,
-        required: [true, 'Product description is required'],
-        trim: true,
-        minlength: [20, 'Product description must be at least 20 characters']
+      type: String,
+      required: [true, "Product description is required"],
+      minlength: [20, "Product description must be at least 20 characters"],
     },
     quantity: {
-        type: Number
+      type: Number,
+      required: [true, "Product quantity is required"],
     },
     sold: {
-        type:Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
-    price:{
-        type:Number,
-        required: [true, 'Product price is required'],
-        trim: true,
-        max: [200000, 'Product price must not exceed 15 characters'],
+    price: {
+      type: Number,
+      required: [true, "Product price is required"],
+      trim: true,
+      max: [20, "Product price must not exceed 20 characters"],
     },
-    priceAfterDiscount:{
-        type:Number,
-        trim: true,
-        default: 0
+    priceAfterDiscount: {
+      type: Number,
+      trim: true,
+      default: 0,
     },
-    colors:[String],
-    imageCover:{
-        type: String,
-        required: [true, 'Product image is required'],
+    colors: [String],
+    imageCover: {
+      type: String,
+      required: [true, "Product image is required"],
     },
     images: [String],
-    category:{
-        type:mongoose.Schema.ObjectId,
-        ref: 'Category',
-        required: [true, 'Category is required']
+    category: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Category",
+      required: [true, "Product must be belong to category"],
     },
-    subcategories:[{
-        type:mongoose.Schema.ObjectId,
-        ref: 'SubCategory',
-    }],
-    brand:{
-        type:mongoose.Schema.ObjectId,
-        ref: 'Brand'
+    subcategories: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "SubCategory",
+      },
+    ],
+    brand: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Brand",
     },
-    ratingsAverage:{
-        type:Number,
-        min: [1, 'Rating must be at least 1'],
-        max: [5, 'Rating must not exceed 5'],
+    ratingsAverage: {
+      type: Number,
+      min: [1, "Rating must be at least 1"],
+      max: [5, "Rating must not exceed 5"],
     },
-    ratingsQuantity:{
-        type:Number,
-        default: 0
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
     },
-},
-{timestamps: true}
+  },
+  { timestamps: true }
 );
 
-productSchema.pre(/^find/, function(next){
-    this.populate({
-        path: 'category',
-        select: 'name -_id'
-    });
-    next();
+productSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "category",
+    select: "name -_id",
+  });
+  next();
 });
 
 // update , delete, get , getALL
-productSchema.post('init', (doc)=>{
-    if(doc.imageCover){
-        const imageUrl = `${process.env.BASE_URL}/products/${doc.imageCover}`;
-        doc.imageCover = imageUrl;
-    }
-    if(doc.images){
-        const images = [];
-        doc.images.forEach((image)=>{
-            const imageUrl = `${process.env.BASE_URL}/products/${image}`;
-            images.push(imageUrl);
-        });
-        doc.images = images;
-    }
+productSchema.post("init", (doc) => {
+  if (doc.imageCover) {
+    const imageUrl = `${process.env.BASE_URL}/products/${doc.imageCover}`;
+    doc.imageCover = imageUrl;
+  }
+  if (doc.images) {
+    const images = [];
+    doc.images.forEach((image) => {
+      const imageUrl = `${process.env.BASE_URL}/products/${image}`;
+      images.push(imageUrl);
+    });
+    doc.images = images;
+  }
 });
 
 // create
-productSchema.post('save', (doc)=>{
-    if(doc.imageCover){
-        const imageUrl = `${process.env.BASE_URL}/products/${doc.imageCover}`;
-        doc.imageCover = imageUrl;
-    }
-    if(doc.images){
-        const images = [];
-        doc.images.forEach((image)=>{
-            const imageUrl = `${process.env.BASE_URL}/products/${image}`;
-            images.push(imageUrl);
-        });
-        doc.images = images;
-    }
+productSchema.post("save", (doc) => {
+  if (doc.imageCover) {
+    const imageUrl = `${process.env.BASE_URL}/products/${doc.imageCover}`;
+    doc.imageCover = imageUrl;
+  }
+  if (doc.images) {
+    const images = [];
+    doc.images.forEach((image) => {
+      const imageUrl = `${process.env.BASE_URL}/products/${image}`;
+      images.push(imageUrl);
+    });
+    doc.images = images;
+  }
 });
 
-const ProductModel = mongoose.model('Product', productSchema);
-
+const ProductModel = mongoose.model("Product", productSchema);
 
 module.exports = ProductModel;
