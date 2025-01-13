eb6337c6f72c0647458ef78017e08515957ec020
mohyaldeentellawi <mohyaldeentellawi@gmail.com>
2025-01-13 17:38:56 +0300

    Refactor product routes and services for improved readability and consistency

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,19 +1,50 @@
-const express = require('express');
-const { getProduct, getProducts, createProduct, updateProduct, deleteProduct, uploadProductImages, productImageProcessing } = require('../services/productServices');
-const { protect, allowTo } = require('../services/authServices');
-const { getProdcutValidator, createProductValidator, updateProductValidator, deleteProductValidator } = require('../utils/validators_rules/productValidators');
+const express = require("express");
+const {
+  getProduct,
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  uploadProductImages,
+  productImageProcessing,
+} = require("../services/productServices");
+const { protect, allowTo } = require("../services/authServices");
+const {
+  getProdcutValidator,
+  createProductValidator,
+  updateProductValidator,
+  deleteProductValidator,
+} = require("../utils/validators_rules/productValidators");
 const router = express.Router();
 
-router.route('/').get(getProducts).post(
-    protect, allowTo("admin", 'manager'),
-    uploadProductImages, productImageProcessing, createProductValidator, createProduct);
-router.route('/:id')
-    .get(getProdcutValidator, getProduct)
-    .put(
-        protect, allowTo("admin", 'manager'),
-        updateProductValidator, updateProduct)
-    .delete(
-        protect, allowTo("admin", 'manager'),
-        deleteProductValidator, deleteProduct);
+router
+  .route("/")
+  .get(getProducts)
+  .post(
+    protect,
+    allowTo("admin", "manager"),
+    uploadProductImages,
+    productImageProcessing,
+    createProductValidator,
+    createProduct
+  );
+router
+  .route("/:id")
+  .get(getProdcutValidator, getProduct)
+  .put(
+    protect,
+    allowTo("admin", "manager"),
+
+    uploadProductImages,
+    productImageProcessing,
+    updateProductValidator,
+    updateProduct
+  )
+  .delete(
+    protect,
+    allowTo("admin", "manager"),
+    deleteProductValidator,
+    deleteProduct
+  );
 
 module.exports = router;
--- services/productServices.js
+++ services/productServices.js
@@ -1,50 +1,49 @@
-const ProductModel = require('../models/productModel');
-const asyncHandler = require('express-async-handler');
-const sharp = require('sharp');
-const { v4: uuidv4 } = require('uuid');
-const {uploadMultipleImages} = require('../middleware/uploadImageMiddleware');
-const factory = require('./handlersFactory');
-
+const ProductModel = require("../models/productModel");
+const asyncHandler = require("express-async-handler");
+const sharp = require("sharp");
+const { v4: uuidv4 } = require("uuid");
+const { uploadMultipleImages } = require("../middleware/uploadImageMiddleware");
+const factory = require("./handlersFactory");
 
 exports.uploadProductImages = uploadMultipleImages([
-    { name: 'imageCover', maxCount: 1 },
-    { name: 'images', maxCount: 5 },
+  { name: "imageCover", maxCount: 1 },
+  { name: "images", maxCount: 5 },
 ]);
 
 // @desc   Image processing for image cover and images for product
 exports.productImageProcessing = asyncHandler(async (req, res, next) => {
-    if(req.files.imageCover){
-        const imageCoverFileName = `product-${uuidv4()}-${Date.now()}-cover.jpeg`;
+  if (req.files.imageCover) {
+    const imageCoverFileName = `product-${uuidv4()}-${Date.now()}-cover.jpeg`;
     await sharp(req.files.imageCover[0].buffer)
-    .resize(2000,1333)
-    .toFormat('jpeg')
-    .jpeg({ quality: 95 })
-    .toFile(`uploads/products/${imageCoverFileName}`);
+      .resize(2000, 1333)
+      .toFormat("jpeg")
+      .jpeg({ quality: 95 })
+      .toFile(`uploads/products/${imageCoverFileName}`);
     // Save the image cover name in Database
-        req.body.imageCover = imageCoverFileName;
-    }
-    if(req.files.images) {
-        req.body.images = [];
-        await Promise.all(
-            req.files.images.map(async(image,index)=>{
-                const imageName = `product-${uuidv4()}-${Date.now()}-${index+1}.jpeg`;
-                await sharp(image.buffer)
-                    .resize(2000,1333)
-                    .toFormat('jpeg')
-                    .jpeg({ quality: 95 })
-                    .toFile(`uploads/products/${imageName}`);
-                    // Save the images name in Database
-                    req.body.images.push(imageName);
-            }));
-        }
-        next();
+    req.body.imageCover = imageCoverFileName;
+  }
+  if (req.files.images) {
+    req.body.images = [];
+    await Promise.all(
+      req.files.images.map(async (image, index) => {
+        const imageName = `product-${uuidv4()}-${Date.now()}-${index + 1}.jpeg`;
+        await sharp(image.buffer)
+          .resize(2000, 1333)
+          .toFormat("jpeg")
+          .jpeg({ quality: 95 })
+          .toFile(`uploads/products/${imageName}`);
+        // Save the images name in Database
+        req.body.images.push(imageName);
+      })
+    );
+  }
+  next();
 });
 
 // @desc   Get all products
 // @route  GET /api/v1/products
 // @access Public
-exports.getProducts = factory.getAll(ProductModel, 'Product');
-
+exports.getProducts = factory.getAll(ProductModel, "Product");
 
 // @desc   Get a specific product
 // @route  GET /api/v1/products/:id
@@ -58,12 +57,9 @@ exports.createProduct = factory.createOne(ProductModel);
 // @desc   Update a product
 // @route  PUT /api/v1/products/:id
 // @access Private
-exports.updateProduct = factory.updateOne(ProductModel); 
+exports.updateProduct = factory.updateOne(ProductModel);
 
 // @desc   Delete a product
 // @route  DELETE /api/v1/products/:id
 // @access Private
 exports.deleteProduct = factory.deleteOne(ProductModel);
-
-
-
