909c28f8061e09b2413e19678253aac03d468960
mohyaldeentellawi <mohyaldeentellawi@gmail.com>
2025-01-13 18:01:46 +0300

    Refactor category model for improved readability and add pre-delete hooks for subcategories and products

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,43 +1,56 @@
-const mongoose = require('mongoose');
-
-
+const mongoose = require("mongoose");
 
 // Create a Schema
-const categorySchema = new mongoose.Schema({
+const categorySchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Category name is required'],
-        trim: true,
-        unique: [true, 'Category name must be unique'],
-        maxlength: [50, 'Category name must not exceed 50 characters'],
-        minlenght: [4, 'Category name must be at least 3 characters']
+      type: String,
+      required: [true, "Category name is required"],
+      trim: true,
+      unique: [true, "Category name must be unique"],
+      maxlength: [50, "Category name must not exceed 50 characters"],
+      minlenght: [4, "Category name must be at least 3 characters"],
     },
     slug: {
-        type: String,
-        lowercase: true
+      type: String,
+      lowercase: true,
     },
     image: String,
-}, { timestamps: true });
-
+  },
+  { timestamps: true }
+);
 
 // update , delete, get , getALL
-categorySchema.post('init', (doc) => {
-    if (doc.image) {
-        const imageUrl = `${process.env.BASE_URL}/categories/${doc.image}`;
-        doc.image = imageUrl;
-    }
+categorySchema.post("init", (doc) => {
+  if (doc.image) {
+    const imageUrl = `${process.env.BASE_URL}/categories/${doc.image}`;
+    doc.image = imageUrl;
+  }
 });
 
 // create
-categorySchema.post('save', (doc) => {
-    if (doc.image) {
-        const imageUrl = `${process.env.BASE_URL}/categories/${doc.image}`;
-        doc.image = imageUrl;
-    }
+categorySchema.post("save", (doc) => {
+  if (doc.image) {
+    const imageUrl = `${process.env.BASE_URL}/categories/${doc.image}`;
+    doc.image = imageUrl;
+  }
 });
 
+// delete all subCategories childs when category is deleted
+categorySchema.pre("findOneAndDelete", async function (next) {
+  const categoryId = this.getQuery()._id;
+  await mongoose.model("SubCategory").deleteMany({ category: categoryId });
+  next();
+});
+
+// delete all products childs when category is deleted
+categorySchema.pre("findOneAndDelete", async function (next) {
+  const productId = this.getQuery()._id;
+  await mongoose.model("Product").deleteMany({ category: productId });
+  next();
+});
 
 // Create a Model
-const CategoryModel = mongoose.model('Category', categorySchema);
+const CategoryModel = mongoose.model("Category", categorySchema);
 
 module.exports = CategoryModel;
