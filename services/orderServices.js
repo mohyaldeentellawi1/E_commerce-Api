4c6f2a63333013934b0fafc8db68607e773d9f10
mohyaldeentellawi <mohyaldeentellawi@gmail.com>
2025-01-21 01:24:54 +0300

    Fix order price calculation in checkout session

--- services/orderServices.js
+++ services/orderServices.js
@@ -164,7 +164,7 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
 const createCardOrder = async (session) => {
   const cartId = session.client_reference_id;
   const shippingAddress = session.metadat;
-  const orderPrice = session.display_items[0].amount / 100;
+  const orderPrice = session.amount_total / 100;
   const cart = await CartModel.findById(cartId);
   const user = await UserModel.findOne({ email: session.customer_email });
 
